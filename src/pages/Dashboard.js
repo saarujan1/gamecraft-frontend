5b2d3b25d915bb42b879f1458c79310bcc26721e
shialis <126681215+shialis@users.noreply.github.com>
2023-12-28 01:52:20 +0200

    updated Homepage and Dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Table } from 'antd';
 
 const Dashboard = () => {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    // Fetch the games from your backend
+    fetch('/api/games')
+      .then(response => response.json())
+      .then(data => setGames(data));
+  }, []);
+
+  const columns = [
+    { title: 'Game Name', dataIndex: 'name', key: 'name' },
+    { title: 'Developer', dataIndex: 'developer', key: 'developer' },
+    { title: 'Description', dataIndex: 'description', key: 'description' },
+  ];
+
   return (
-    <>
-  <div>Dashboard Content</div>
-  </>
+    <div>
+      <h2>Dashboard</h2>
+      <Table dataSource={games} columns={columns} />
+    </div>
   );
 };
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const HomePage = () => {
+  const [registerUsername, setRegisterUsername] = useState('');
+  const [registerPassword, setRegisterPassword] = useState('');
+  const [loginUsername, setLoginUsername] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+    // Handle the registration logic here
+    console.log('Registering:', { registerUsername, registerPassword });
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    // Handle the login logic here
+    console.log('Logging in:', { loginUsername, loginPassword });
+  };
+
   return (
-    <>
-  <div>Home Page Content</div>
-  </>
+    <div>
+      <h2>Register</h2>
+      <form onSubmit={handleRegister}>
+        <input type="text" value={registerUsername} onChange={(e) => setRegisterUsername(e.target.value)} placeholder="Username" required />
+        <input type="password" value={registerPassword} onChange={(e) => setRegisterPassword(e.target.value)} placeholder="Password" required />
+        <button type="submit">Register</button>
+      </form>
+
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <input type="text" value={loginUsername} onChange={(e) => setLoginUsername(e.target.value)} placeholder="Username" required />
+        <input type="password" value={loginPassword} onChange={(e) => setLoginPassword(e.target.value)} placeholder="Password" required />
+        <button type="submit">Login</button>
+      </form>
+    </div>
   );
 };
 
